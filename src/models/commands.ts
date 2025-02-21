f446081a06e8e3d1734e98ed428919314d7356d4
33b3ziz <moabdelaziz3225@gmail.com>
2025-02-21 22:18:01 +0200

    -

--- src/models/commands.ts
+++ src/models/commands.ts
@@ -1,94 +1,106 @@
-import build from './build'
-import build_landing_page from './build-landing-page'
-import help from './help'
-import version from './version'
-import new_ from './new'
-import login from './login'
-import list from './list'
-import preview from './preview'
-import update from './update'
-
+import build from "./build";
+import build_landing_page from "./build-landing-page";
+import help from "./help";
+import version from "./version";
+import new_ from "./new";
+import login from "./login";
+import list from "./list";
+import preview from "./preview";
+import update from "./update";
 
 interface CLICommands {
-    name: string; 
-    command: string; 
-    examples: string[];
+  name: string;
+  command: string;
+  examples: string[];
 }
 
-
-const CLI_Commands:Set<String>= new Set([
-    'version',
-    'v',
-    '--version',
-    '--v',
-    'build',
-    'build-landing-page',
-    'new',
-    'help',
-    'login',
-    'list',
-    'preview',
-    'update',
+const CLI_Commands: Set<String> = new Set([
+  "version",
+  "v",
+  "--version",
+  "--v",
+  "build",
+  "build-landing-page",
+  "new",
+  "help",
+  "login",
+  "list",
+  "preview",
+  "update",
 ]);
 
 const commands: Array<CLICommands> = [
-    {
-        name: "build",
-        command: "build args[ --name optional => default:cwd name, --path optional => default:cwd ]",
-        examples: ["example-1: zid-theme build --name omar --path ./folder", "example-2: zid-theme build"]
-    },
-    {
-        name: "build-landing-page",
-        command: "build-landing-page args[ --name optional => default:cwd name, --path optional => default:cwd ]",
-        examples: ["example-1: zid-theme build --name omar --path ./folder", "example-2: zid-theme build"]
-    },
-    {
-        name: "new",
-        command: "new args[ theme name: name_your_theme]",
-        examples: ["example: zid-theme new best_theme"]
-    },
-    {
-        name: "help",
-        command: "help",
-        examples: ["example: zid-theme help"]
-    },
-    {
-        name: "version",
-        command: "[version, v, --version, --v]",
-        examples: ["example: zid-theme --v"]
-    },
-    {
-        name: "login",
-        command: "login",
-        examples: ["example: zid-theme login"]
-    },
-    {
-        name: "list",
-        command: "list",
-        examples: ["example: zid-theme list"]
-    },
-    {
-        name: "preview",
-        command: "preview args[ --store_email required --theme_name optional]",
-        examples: ["example: zid-theme preview --store_email sqwiktpoo1334@zam-partner.email --theme_name 'my awesome theme'" ],
-    },
-    {
-        name: "update",
-        command: "update args[ --theme_id required, --change_type required, --release_notes required ]",
-        examples: ["example: zid-theme update --theme_id 1234 --change_type major --release_notes 'this is a major update'"],
-    },
+  {
+    name: "build",
+    command:
+      "build args[ --name optional => default:cwd name, --path optional => default:cwd ]",
+    examples: [
+      "example-1: zid-theme build --name omar --path ./folder",
+      "example-2: zid-theme build",
+    ],
+  },
+  {
+    name: "build-landing-page",
+    command:
+      "build-landing-page args[ --name optional => default:cwd name, --path optional => default:cwd ]",
+    examples: [
+      "example-1: zid-theme build --name omar --path ./folder",
+      "example-2: zid-theme build",
+    ],
+  },
+  {
+    name: "new",
+    command: "new args[ theme name: name_your_theme]",
+    examples: ["example: zid-theme new best_theme"],
+  },
+  {
+    name: "help",
+    command: "help",
+    examples: ["example: zid-theme help"],
+  },
+  {
+    name: "version",
+    command: "[version, v, --version, --v]",
+    examples: ["example: zid-theme --v"],
+  },
+  {
+    name: "login",
+    command: "login",
+    examples: ["example: zid-theme login"],
+  },
+  {
+    name: "list",
+    command: "list",
+    examples: ["example: zid-theme list"],
+  },
+  {
+    name: "preview",
+    command:
+      "preview args[ --store_email required --theme_name optional --theme-url optional]",
+    examples: [
+      "example: zid-theme preview --store_email sqwiktpoo1334@zam-partner.email --theme_name 'my awesome theme' --theme-url 'https://my-awesome-theme.com'",
+    ],
+  },
+  {
+    name: "update",
+    command:
+      "update args[ --theme_id required, --change_type required, --release_notes required ]",
+    examples: [
+      "example: zid-theme update --theme_id 1234 --change_type major --release_notes 'this is a major update'",
+    ],
+  },
 ];
 
 export {
-    commands,
-    build,
-    build_landing_page,
-    new_,
-    help,
-    version,
-    login,
-    list,
-    preview,
-    update,
-    CLI_Commands
-}
+  commands,
+  build,
+  build_landing_page,
+  new_,
+  help,
+  version,
+  login,
+  list,
+  preview,
+  update,
+  CLI_Commands,
+};
